159965839a976168338929f9e8610ca95192dc0b
Renju Radhakrishnan <renju467@gmail.com>
2018-03-20 21:47:55 -0400

    use tmp text file instead

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -3,10 +3,31 @@ package main
 import (
 	"testing"
 
+	"io/ioutil"
+
+	"bytes"
+
+	"time"
+
 	"github.com/sirupsen/logrus"
 )
 
 func Test_ReadFile(t *testing.T) {
-	commands := FileReader("/Users/radhakrishnanr1/go/src/github.com/renjuju/go-parallel/test.txt")
+	file, err := ioutil.TempFile("/tmp/", "paralleling")
+	if err != nil {
+		t.Error(err)
+	}
+
+	buf := bytes.NewBufferString("ls -la\npwd | pbcopy")
+
+	file.Write(buf.Bytes())
+	err = file.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	commands := FileReader(file.Name())
 	logrus.Info(commands)
+
+	time.Sleep(100 * time.Millisecond) // why are these tests run as go routines?
 }
